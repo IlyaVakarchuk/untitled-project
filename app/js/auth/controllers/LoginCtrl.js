b5b110cb2baf2267a638e78705304a340983d7e1
Ilya Vakarchuk <ilyavakarchuk@gmail.com>
2016-06-07 19:56:58 +0600

    add panel and logic for registration; new visual effects

--- app/js/auth/controllers/LoginCtrl.js
+++ app/js/auth/controllers/LoginCtrl.js
@@ -3,20 +3,40 @@ app.controller('LoginCtrl',
   function ($scope, $resource){
     var req = $resource('/api/auth');
 
-    $scope.login = function() {
-      req.get();
-    };
+    $scope.switch = 'login';
 
-    $scope.registartion = function(username, pass01, pass02) {
-      if ((username !== undefined) && (pass01 !== undefined) && (pass02 !== undefined) && (pass01 === pass02)) {
-        req.save({ username : username, password : pass01 }).$promise.then(function(data) {
+    $scope.switchPanel = function(panel) {
+      $scope.switch = panel;
+    }
+
+    $scope.login = function(username, password) {
+      console.log(username)
+      console.log(password)
+      if ((username !== undefined) && (password !== undefined)) {
+        /*req.save({ username : username, password : pass01 }).$promise.then(function(data) {
           console.log(data);
         }, function(data) {
           console.log(data);
-        });
+        }); */
+
       } else {
 
       }
+    };
+
+    $scope.registartion = function(username, pass01, pass02) {
+      console.log(username)
+      console.log(pass01)
+      console.log(pass02)
+      // if ((username !== undefined) && (pass01 !== undefined) && (pass02 !== undefined) && (pass01 === pass02)) {
+      //   req.save({ username : username, password : pass01 }).$promise.then(function(data) {
+      //     console.log(data);
+      //   }, function(data) {
+      //     console.log(data);
+      //   });
+      // } else {
+
+      // }
     };
 
     $scope.logout = function() {
